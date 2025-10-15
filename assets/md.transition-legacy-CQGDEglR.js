1264223708ff9e2eea377df52173f86aeca6aa6d
tqdaoinfo108 <50355596+tqdaoinfo108@users.noreply.github.com>
2025-10-15 04:31:56 +0000

    Deploying to gh-pages from @ tqdaoinfo108/wayos_ionic@5299b110e17ee8f6165fb6adf99951361a40da73 🚀

--- assets/md.transition-legacy-CQGDEglR.js
+++ assets/md.transition-legacy-CQGDEglR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BLONmXqT.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BeaI_vi7.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
